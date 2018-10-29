cd477b08ae4c5bc6c031eb3a39f28d7cfac4fe16
gjtinoco <genaro_13@hotmail.es>
2018-10-29 09:10:08 -0600

    Finalizacion proyecto (faltan prop types)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header';
 import Noticias from '../Noticias';
+import Formulario from '../Formulario';
 
 class App extends Component {
 
-  state= {
-    noticias: []
+  constructor( props ) {
+    super(props);
+    this.state= {
+      noticias: [],
+      categoria:'general'
+    }
+
   }
   componentDidMount() {
     this.consultarNoticias();
   }
 
-  consultarNoticias = () => {
-    let url = 'https://newsapi.org/v2/top-headlines?country=mx&category=general&apiKey=1976374f9d1446b290be434e147ea4de';
+  consultarNoticias = ( categoria = 'general' ) => {
+    this.setState({
+      categoria: categoria
+    })
+    
+    let url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=1976374f9d1446b290be434e147ea4de`;
+console.log(url);
 
     let myInit = {
       method:'GET'
@@ -33,6 +44,8 @@ class App extends Component {
       <div className="contenedor-app">
         <Header titulo="Noticias"/>
         <div className="container white contenedor-noticias">
+          <Formulario 
+            consultarNoticias={this.consultarNoticias}/>
           <Noticias 
             noticias={this.state.noticias}
             />
--- src/components/Noticias/Noticias.js
+++ src/components/Noticias/Noticias.js
@@ -1,16 +1,24 @@
 import React,{Component} from 'react';
 import Noticia from '../Noticia/Noticia';
-
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 export default class Noticias extends Component {
 
     render() {
         return (
             <div className="row">
-               {
-                this.props.noticias.map((noticia, index) => {
-                    return <Noticia noticia={noticia} key={noticia.url}/>  
-                })
+                <TransitionGroup>
+                {
+                    this.props.noticias.map((noticia, index) => {
+                        return <CSSTransition
+                                    key={noticia.url}
+                                    classNames="fade"
+                                    timeout={700}
+                                    >
+                                    <Noticia noticia={noticia}/>  
+                                </CSSTransition>
+                    })
                 }
+                </TransitionGroup>
             </div>
         );
     }
